2f94fc0a032c5021d089e7fbbc0baf1bda991ac3
felipe-bitt <felipebittfernandes@gmail.com>
2021-08-11 18:50:23 -0300

    create a job and a callback(still templates)

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,11 @@
 class Pet < ApplicationRecord
+  validates :name, presence: true
   belongs_to :user
+
+  after_create :add_two_pets
+
+
+  def add_two_pets
+    puts "Bora criar mais dois pet fanfarrao?"
+  end
 end
